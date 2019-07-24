c272268baf16350a1bfa56fc84a4760a48dc997a
Elle Luo <el685@cornell.edu>
2019-07-24 09:06:28 -0400

    d7 project setup.

---
+++ d7/src/d7/SortingTests.java
@@ -0,0 +1,14 @@
+package d7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SortingTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
