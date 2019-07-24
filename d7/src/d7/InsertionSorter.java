3890153c458f7d201b3d103202c1bf418362f9e6
Elle Luo <el685@cornell.edu>
2019-07-24 09:42:52 -0400

    created InsertionSorter class and added constructor.

---
+++ d7/src/d7/InsertionSorter.java
@@ -0,0 +1,12 @@
+package d7;
+
+import java.util.Comparator;
+
+public abstract class InsertionSorter<E> extends Sorter<E>{
+
+	public InsertionSorter(Comparator<E> cmp) {
+		super(cmp);
+		// TODO Auto-generated constructor stub
+	}
+
+}
